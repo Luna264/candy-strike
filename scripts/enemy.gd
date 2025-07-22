8422b17e2d98634f89e458d680fed18604bda1a1
Luna264 <ameerawon@gmail.com>
2025-07-22 16:14:22 -0400

    first enemy ai, enemy flash effect, replacing animated sprite 2d with animation player, (bugged) animation playing

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,36 +2,101 @@ extends CharacterBody2D
 
 var health = 150
 @export var speed = -80.0
-var friction = 500.0 
+@onready var player = get_tree().get_first_node_in_group("player")
+var friction = 500.0
 var damageoutput = 150
+var knockback_x_jump = 200
+var knockback_y_jump = -200
+
+@onready var jump_timer: Timer = $JumpTimer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var attack_timer: Timer = $AttackTimer
+@onready var damage_timer: Timer = $DamageTimer
+
+var is_attacking = false
+var is_damaged = false
+var flash_in_progress = false
 
 signal damage_output
 
+
+func _ready() -> void:
+	sprite_2d.material = sprite_2d.material.duplicate()
+
+
 func _process(delta: float) -> void:
+	if not is_attacking and not is_damaged:
+		animation_player.play("idle")
+
 	if health <= 0:
 		print("enemy dead")
 		queue_free()
-		
+
+
 func take_damage(dmg, attacker_position, knockback_x, knockback_y):
-	health = health - dmg
-	var direction = (global_position - attacker_position).normalized() #normalize = direction, just want to know where its pointing, not the length
-	velocity.x = direction.x * knockback_x #multiply direction,where its pointing, by knockback power
-	velocity.y = knockback_y #and for y axis too
+	if is_damaged:
+		return 
 	
+	is_damaged = true
+	damage_timer.start()
+	flash()
+	health -= dmg
+
+#knockback
+	var direction = (global_position - attacker_position).normalized()
+	velocity.x = direction.x * knockback_x
+	velocity.y = knockback_y
+
+
 func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if is_on_floor() and velocity.y > 0:
+	elif velocity.y > 0:
 		velocity.y = 0 
-	
-	if is_on_floor():
-		velocity.x = speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, friction * delta) #from, to, by amount of friction
+
+
+	velocity.x = move_toward(velocity.x, 0, friction * delta)
 	move_and_slide()
 
 
-func _on_hitbox_area_entered(area: Area2D) -> void: #pass in damage
-	emit_signal("damage_output", 50)
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	emit_signal("damage_output", 10)
+
+
+func _slime_jump() -> void:
+	is_attacking = true
+	attack_timer.start()
+	face_player()
+	var direction = (player.global_position - global_position).normalized()
+	velocity.x = direction.x * knockback_x_jump
+	velocity.y = knockback_y_jump
+
+
+func _on_jump_timer_timeout() -> void:
+	if player:
+		_slime_jump()
+
+
+func face_player():
+	if player:
+		sprite_2d.flip_h = player.global_position.x < global_position.x
+
+
+func _on_attack_timer_timeout() -> void:
+	is_attacking = false
+
+
+func _on_damage_timer_timeout() -> void:
+	is_damaged = false
+
+
+func flash():
+	if flash_in_progress:
+		return 
+	flash_in_progress = true
 	
+	animation_player.play("hit")
+	
+	flash_in_progress = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,42 +1,53 @@
 extends CharacterBody2D
 
 @onready var coyote_timer: Timer = $CoyoteTimer
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var buffer_timer: Timer = $BufferTimer
 @onready var candycane: Area2D = $candycane
 @onready var anim_player = candycane.get_node("AnimationPlayer")
 @onready var attack: Timer = $Attack
 @onready var cooldown_crit: Timer = $CooldownCrit
+@onready var _player_animation_player: AnimationPlayer = $AnimationPlayer
+@onready var damage_timer: Timer = $DamageTimer
+
+enum PlayerState { IDLE, RUN, JUMP, ATTACK, HIT }
+var current_state = PlayerState.IDLE
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 var is_crit = false
 var is_attacking = false
+var is_damaged = false
 var friction = 500.0
 var knockback = Vector2.ZERO
 var knockback_toggle = false
 var knockback_timer = 0.0
+var flash_in_progress = false
 
 var health = 150
 
 func _process(delta: float) -> void:
+	
+	set_animation()
 #ATTACKING
 	if Input.is_action_just_pressed("b_attack"):
+		current_state = PlayerState.ATTACK
 		is_crit = false
 		is_attacking = true
-		if animated_sprite_2d.flip_h == false:
+		if sprite_2d.flip_h == false:
 			anim_player.play("b_attack_right")
-		if animated_sprite_2d.flip_h == true:
+		if sprite_2d.flip_h == true:
 			anim_player.play("b_attack_left")
 		attack.start()
 
 		
 	if Input.is_action_just_pressed("c_attack") and cooldown_crit.is_stopped():
+		current_state = PlayerState.ATTACK
 		is_crit = true
 		is_attacking = true
-		if animated_sprite_2d.flip_h == false:
+		if sprite_2d.flip_h == false:
 			anim_player.play("c_attack_right")
-		if animated_sprite_2d.flip_h == true:
+		if sprite_2d.flip_h == true:
 			anim_player.play("c_attack_left")
 		attack.start()
 		cooldown_crit.start()
@@ -47,12 +58,12 @@ func _process(delta: float) -> void:
 func _on_attack_timeout() -> void:
 	is_attacking = false
 	anim_player.play("RESET")
-	
 
 var was_on_floor = false
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
+	
 	
 	
 	if knockback_timer > 0.0: #knockback enabled
@@ -76,6 +87,7 @@ func _physics_process(delta: float) -> void:
 		buffer_timer.start()
 				
 	if Input.is_action_just_pressed("jump") and is_on_floor() || Input.is_action_just_pressed("jump") and not coyote_timer.is_stopped() ||  not buffer_timer.is_stopped() && is_on_floor():
+		current_state = PlayerState.JUMP
 		velocity.y = JUMP_VELOCITY
 		buffer_timer.stop()
 		coyote_timer.stop()
@@ -84,19 +96,22 @@ func _physics_process(delta: float) -> void:
 	if knockback_toggle == false: #move when knockback is not enabled
 		if direction:
 			velocity.x = direction * SPEED
+			
 		else:
 			velocity.x = move_toward(velocity.x, 0, friction * delta)
-				
+			
+		if not is_attacking and not is_damaged:
+			current_state = PlayerState.IDLE		
 			#direction flipping
 		if direction == 1 and is_attacking == false:
-			animated_sprite_2d.flip_h = false
+			sprite_2d.flip_h = false
 			anim_player.play("idle_right")
 		if direction == -1 and is_attacking == false:
-			animated_sprite_2d.flip_h = true
+			sprite_2d.flip_h = true
 			anim_player.play("idle_left")
 	elif knockback_toggle == true:
 		direction = 0 #no moving when knockback is enabled
-		
+		current_state = PlayerState.IDLE
 	move_and_slide()
 		
 	
@@ -111,7 +126,38 @@ func _on_hurtbox_area_entered(area: Area2D) -> void:
 		_knockback(area.get_parent().position)
 		print("collided")
 
-
-
-func _on_enemy_damage_output(damage_output) -> void: #damage to play
+func _on_enemy_damage_output(damage_output) -> void: #take_damage function for player basically
+	if is_damaged:
+		return 
+	
+	is_damaged = true
+	damage_timer.start()
+	flash()
 	health = health - damage_output
+	
+	
+func set_animation():
+	if current_state == PlayerState.HIT:
+		_player_animation_player.play("hit")
+		
+	if current_state == PlayerState.JUMP:
+		_player_animation_player.play("jump")
+		
+	if current_state == PlayerState.IDLE:
+		_player_animation_player.play("idle")
+		
+	if current_state == PlayerState.RUN:
+		_player_animation_player.play("run")
+	
+func flash():
+	if flash_in_progress:
+		return 
+	flash_in_progress = true
+	
+	_player_animation_player.play("hit")
+	
+	flash_in_progress = false
+
+func _on_damage_timer_timeout() -> void:
+	is_damaged = false
+	
