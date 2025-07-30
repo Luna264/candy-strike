43d5ee008a4f36493c25e9f814c9ef8519ec906f
Luna264 <ameerawon@gmail.com>
2025-07-29 21:11:19 -0400

    breaking glass platforms

---
+++ scripts/glassplatform.gd
@@ -0,0 +1,42 @@
+extends AnimatableBody2D
+
+var breaking_timer: float = 0.0
+var is_breaking = false
+var is_shattering = false
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+signal shatter
+
+func _physics_process(delta: float) -> void:
+	if is_breaking == true:
+		breaking_timer += delta
+		
+	if is_breaking == false and is_shattering == false:
+		breaking_timer -= delta
+		animation_player.play("idle")
+	
+	if breaking_timer > 3 and not is_shattering:
+		animation_player.play("breaking")
+		
+	if breaking_timer > 6:
+		is_shattering = true
+		emit_signal("shatter")
+		
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player") or area.is_in_group("enemy"):
+		is_breaking = true
+		print("HERE")
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player") or area.is_in_group("enemy"):
+		is_breaking = false
+		print("GONE")
+
+func _on_shatter() -> void:
+	animation_player.play("shattering")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "shattering":
+		queue_free()
---
+++ scripts/whip.gd
@@ -0,0 +1,89 @@
+extends CharacterBody2D
+
+var health = 150
+@export var speed = -80.0
+@onready var player = get_tree().get_first_node_in_group("player")
+var friction = 500.0
+var damageoutput = 150
+var knockback_x_jump = 200
+var knockback_y_jump = -200
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var attack_timer: Timer = $AttackTimer
+@onready var damage_timer: Timer = $DamageTimer
+
+
+var is_attacking = false
+var is_damaged = false
+var flash_in_progress = false
+
+signal damage_output
+signal shakescreen
+
+func _ready() -> void:
+	sprite_2d.material = sprite_2d.material.duplicate()
+
+
+func _process(delta: float) -> void:
+	if not is_attacking and not is_damaged and not flash_in_progress:
+		animation_player.play("idle")
+
+	if health <= 0:
+		print("enemy dead")
+		queue_free()
+
+
+func take_damage(dmg, attacker_position, knockback_x, knockback_y):
+	emit_signal("shakescreen")
+	if is_damaged or flash_in_progress:
+		return
+	
+	is_damaged = true
+	damage_timer.start()
+	flash()
+	health -= dmg
+
+#knockback
+
+
+func _physics_process(delta):
+
+	move_and_slide()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.name == "hurtbox":
+		emit_signal("damage_output", 10)
+
+
+
+
+func _on_attack_timer_timeout() -> void:
+	is_attacking = false
+	flash_in_progress = false
+	animation_player.play("RESET")
+	
+func _on_damage_timer_timeout() -> void:
+	is_damaged = false
+	flash_in_progress = false
+	animation_player.play("RESET")
+
+func flash():
+	if flash_in_progress:
+		animation_player.stop() # force stop previous to allow restart
+	animation_player.play("hit")
+	flash_in_progress = true
+	
+	
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "hit":
+		flash_in_progress = false
+
+
+func _on_detection_attack() -> void:
+	attack_timer.start()
+
+
+func _on_detection_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
