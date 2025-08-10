1f4ed1caa0d3a8f0e90ffeca7f47777a3452ff7e
Luna264 <ameerawon@gmail.com>
2025-08-10 18:21:37 -0400

    bubble soda sprites and sounds (x and y axis)

--- scripts/bubblysodaXAXIS.gd
+++ scripts/bubblysodaXAXIS.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var floatAmount = -200
+var speed = -200
 var active = false
 @onready var cooldown_timer: Timer = $"../CooldownTimer"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -9,7 +9,7 @@ var active = false
 func _physics_process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 			if body.is_in_group("player") or body.is_in_group("enemy"):
-				body.velocity.y = floatAmount
+				body.velocity.x = speed
 
 func update_animation(animation):
 	animation_player.play(animation)
@@ -17,7 +17,11 @@ func update_animation(animation):
 func _on_cooldown_timer_timeout() -> void:
 	active = !active 
 	update_animation("on" if active else "off")
-
+	
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") or body.is_in_group("enemy"):
-		swoosh.play()
+	if active == true:
+		if body.is_in_group("player") or body.is_in_group("enemy"):
+			swoosh.play()
+	if active == false:
+		if body.is_in_group("player") or body.is_in_group("enemy"):
+			swoosh.play()
