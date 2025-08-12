0fe621377294754a6591e237675880ca66cf3a9a
Luna264 <ameerawon@gmail.com>
2025-08-12 14:58:54 -0400

    tileset revamp, bubble soda bug fixes, added decorations (cookies + donuts)

--- scripts/bubblysoda.gd
+++ scripts/bubblysoda.gd
@@ -6,6 +6,9 @@ var active = false
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var swoosh: AudioStreamPlayer2D = %Swoosh
 
+func _ready() -> void:
+	update_animation("off")
+	
 func _physics_process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 			if body.is_in_group("player") or body.is_in_group("enemy"):
@@ -19,5 +22,5 @@ func _on_cooldown_timer_timeout() -> void:
 	update_animation("on" if active else "off")
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") or body.is_in_group("enemy"):
+	if body.is_in_group("player") or body.is_in_group("enemy") and active == true:
 		swoosh.play()
--- scripts/bubblysodaXAXIS.gd
+++ scripts/bubblysodaXAXIS.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
-var speed = -200
+var floatAmount = -200
 var active = false
 @onready var cooldown_timer: Timer = $"../CooldownTimer"
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var swoosh: AudioStreamPlayer2D = %Swoosh
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	update_animation("off")
+	
 func _physics_process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 			if body.is_in_group("player") or body.is_in_group("enemy"):
-				body.velocity.x = speed
+				body.velocity.x = floatAmount
 
 func update_animation(animation):
 	animation_player.play(animation)
@@ -17,11 +20,7 @@ func update_animation(animation):
 func _on_cooldown_timer_timeout() -> void:
 	active = !active 
 	update_animation("on" if active else "off")
-	
+
 func _on_body_entered(body: Node2D) -> void:
-	if active == true:
-		if body.is_in_group("player") or body.is_in_group("enemy"):
-			swoosh.play()
-	if active == false:
-		if body.is_in_group("player") or body.is_in_group("enemy"):
-			swoosh.play()
+	if body.is_in_group("player") or body.is_in_group("enemy") and active == true:
+		swoosh.play()
